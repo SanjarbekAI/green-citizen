f037b044a945a6186be50aeb1741e72e6dae5997
SanjarbekAI <sanjarbekwork@gmail.com>
2026-04-13 20:41:59 +0500

    done

---
+++ notes.py
@@ -0,0 +1,19 @@
+"""
+Onboarding()
+FAQs()
+Notifications()
+News()
+Events()
+ServiceTypes(name, score:json)
+Services(service_type, name, data:json, status, score, user)
+Steps(user, created_at, updated_at, count)
+
+
+APPS:
+
+news
+events
+services
+steps
+
+"""
